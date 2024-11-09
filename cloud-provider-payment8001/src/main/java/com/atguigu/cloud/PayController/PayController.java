08c66ba0bec7f09a1ca2e4c40755cc39c3bcbad0
wms <wangmushuistudio@gmail.com>
2024-11-09 23:24:43 +0800

    pay模块2.0

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/PayController/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/PayController/PayController.java
@@ -2,41 +2,73 @@ package com.atguigu.cloud.PayController;
 
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.resp.ReturnCodeEnum;
 import com.atguigu.cloud.services.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@Slf4j
-public class PayController {
+@Tag(name = "支付微服务模块",description = "支付CRUD")
+public class PayController
+{
     @Resource
     PayService payService;
 
     @PostMapping(value = "/pay/add")
-    public String addPay(@RequestBody Pay pay) {
+    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
+    public ResultData<String> addPay(@RequestBody Pay pay)
+    {
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值：" + i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    @Operation(summary = "删除",description = "删除支付流水方法")
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
 
     @PutMapping(value = "/pay/update")
-    public String updatePay(@RequestBody PayDTO payDTO) {
+    @Operation(summary = "修改",description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO)
+    {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
 
         int i = payService.update(pay);
-        return "成功修改记录，返回值：" + i;
+        return ResultData.success("成功修改记录，返回值："+i);
     }
 
     @GetMapping(value = "/pay/get/{id}")
-    public Pay getById(@PathVariable("id") Integer id) {
-        return payService.getById(id);
-    }//全部查询getall作为家庭作业
+    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        if(id == -4) throw new RuntimeException("id不能为负数");
+
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    //全部查询getall作为家庭作业
+
+    @RequestMapping(value = "/pay/error",method = RequestMethod.GET)
+    public ResultData<Integer> getPayError()
+    {
+        Integer i = Integer.valueOf(200);
+        try
+        {
+            System.out.println("--------come here");
+            int data = 10/0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+        }
+        return ResultData.success(i);
+    }
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,48 +14,57 @@ import java.util.Date;
  * 表注释：支付交易表
 */
 @Table(name = "t_pay")
+@Schema(title = "支付交易表实体类")
 public class Pay {
     @Id
     @GeneratedValue(generator = "JDBC")
+    @Schema(title = "主键")
     private Integer id;
 
     /**
      * 支付流水号
      */
     @Column(name = "pay_no")
+    @Schema(title = "支付流水号")
     private String payNo;
 
     /**
      * 订单流水号
      */
     @Column(name = "order_no")
+    @Schema(title = "订单流水号")
     private String orderNo;
 
     /**
      * 用户账号ID
      */
     @Column(name = "user_id")
+    @Schema(title = "用户账号ID")
     private Integer userId;
 
     /**
      * 交易金额
      */
+    @Schema(title = "交易金额")
     private BigDecimal amount;
 
     /**
      * 删除标志，默认0不删除，1删除
      */
+
     private Byte deleted;
 
     /**
      * 创建时间
      */
+    @Schema(title = "创建时间")
     @Column(name = "create_time")
     private Date createTime;
 
     /**
      * 更新时间
      */
+    @Schema(title = "更新时间")
     @Column(name = "update_time")
     private Date updateTime;
 
